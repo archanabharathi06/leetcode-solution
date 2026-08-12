b61c290d44f0d9cb27dbb37ef80f1e0ddb004585
archanabharathi06 <archanabharathiarchana@gmail.com>
2026-08-12 11:12:05 +0530

    Create 1791-RichestCustomerWealth

---
+++ 1791-RichestCustomerWealth/1791-RichestCustomerWealth.java
@@ -0,0 +1,16 @@
+// Last updated: 8/12/2026, 11:12:04 AM
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int i=0;i<accounts.length;i++){
+            int sum=0;
+            for(int j=0;j<accounts[i].length;j++){
+                sum+=accounts[i][j];
+            }
+            if(sum>max){
+                max=sum;
+            }
+        }
+        return max;
+    }
+}
