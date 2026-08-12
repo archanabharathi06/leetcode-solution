efbc19480657fbdabc108d81686fc0b71e95eee9
archanabharathi06 <archanabharathiarchana@gmail.com>
2026-08-12 11:14:43 +0530

    Create 643-MaximumAverageSubarrayI

---
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.java
@@ -0,0 +1,18 @@
+// Last updated: 8/12/2026, 11:14:42 AM
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        int max=sum;
+        for(int i=k;i<n;i++){
+            sum=sum-nums[i-k]+nums[i];
+            if(sum>max){
+                max=sum;
+            }
+        }
+        return (double) max/k;
+    }
+}
