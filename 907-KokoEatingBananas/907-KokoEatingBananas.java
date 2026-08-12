8684bfb6269b85d36fa0212b3c078f2655522c62
archanabharathi06 <archanabharathiarchana@gmail.com>
2026-08-12 11:13:57 +0530

    Create 907-KokoEatingBananas

---
+++ 907-KokoEatingBananas/907-KokoEatingBananas.java
@@ -0,0 +1,25 @@
+// Last updated: 8/12/2026, 11:13:57 AM
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = 0;
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > high) {
+                high = piles[i];
+            }
+        }
+        while (low < high) {
+            int mid = (low + high) / 2;
+            int hours = 0;
+            for (int i = 0; i < piles.length; i++) {
+                hours += (piles[i] + mid - 1) / mid;
+            }
+            if (hours <= h) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
