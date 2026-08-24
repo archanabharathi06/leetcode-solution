d421026507056b84117df12914e4f14a77a7aec3
archanabharathi06 <archanabharathiarchana@gmail.com>
2026-08-24 15:59:42 +0530

    Create 503-NextGreaterElementII

---
+++ 503-NextGreaterElementII/version/java/503-NextGreaterElementII_20260824_155941.java
@@ -0,0 +1,18 @@
+// Last updated: 8/24/2026, 3:59:41 PM
+1class Solution {
+2    public int[] nextGreaterElements(int[] nums) {
+3        int n=nums.length;
+4        int ans[]=new int[n];
+5        for(int i=0;i<n;i++){
+6            ans[i]=-1;
+7            for(int j=1;j<n;j++){
+8                int k=(i+j)% n;
+9                if(nums[k]>nums[i]){
+10                    ans[i]=nums[k];
+11                    break;
+12                }
+13            }
+14        }
+15        return ans;
+16    }
+17}
