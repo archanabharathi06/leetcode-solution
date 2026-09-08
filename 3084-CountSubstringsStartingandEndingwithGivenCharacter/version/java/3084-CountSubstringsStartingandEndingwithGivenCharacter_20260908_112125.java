f1d406ff054dbd2288c5d1256c36a6d570eb7e61
archanabharathi06 <archanabharathiarchana@gmail.com>
2026-09-08 11:21:26 +0530

    Create 3084-CountSubstringsStartingandEndingwithGivenCharacter

---
+++ 3084-CountSubstringsStartingandEndingwithGivenCharacter/version/java/3084-CountSubstringsStartingandEndingwithGivenCharacter_20260908_112125.java
@@ -0,0 +1,14 @@
+// Last updated: 9/8/2026, 11:21:25 AM
+1class Solution {
+2    public long countSubstrings(String s, char c) {
+3        long count=0;
+4        long ans=0;
+5        for(int i=0;i<s.length();i++){
+6            if(s.charAt(i)==c){
+7                count++;
+8                ans+=count;
+9            }
+10        }
+11        return ans;
+12    }
+13}
